88d94d072b69431f9db571ee749b7d1355b4a617
GodoyMS <peruvalmunidial@gmail.com>
2024-04-23 19:23:28 -0500

    first commit

---
+++ src/constants/data.js
@@ -0,0 +1,97 @@
+export const events=[
+    {
+        id:"1",
+        headerTitle:" Mazatlán, Sin. ",
+        title:"Legion RG - La Malinche - Mazatlán, Sin. ",
+        location:"La Malinche",
+        hour:"08:00pm",
+        date:"27 ",
+        month:"Abr",
+
+        clasifitacion:"+18",
+        image:"https://ideogram.ai/api/images/direct/MmYIGWKfSxSR4qIGK4IShg.jpg",
+    },
+    {
+        id:"2",
+        headerTitle:"Cancún, Q.R.",
+        title:"Beach Bash - Playa Delfines - Cancún, Q.R.",
+        location:"Playa Delfines",
+        hour:"09:30pm",
+        date:"30 ",
+        month:"Abr",
+
+        clasifitacion:"+21",
+        image:"https://ideogram.ai/api/images/direct/BEbWmxsQQ-KMKAw-OTJgrA.jpg",
+    },
+    {
+        id:"3",
+        headerTitle:"Guadalajara, Jal.",
+        title:"Electro Fest - Estadio Akron - Guadalajara, Jal.",
+        location:"Estadio Akron",
+        hour:"10:00pm",
+        date:"05 ",
+        month:"May",
+
+        clasifitacion:"+20",
+        image:"https://ideogram.ai/api/images/direct/4dW4_kwhTAWqObC8savDUg.jpg",
+    },
+    {
+        id:"4",
+        headerTitle:"Puerto Vallarta, Jal.",
+        title:"Salsa Night - Malecón - Puerto Vallarta, Jal.",
+        location:"Malecón",
+        hour:"07:30pm",
+        date:"10 ",
+        month:"May",
+
+        clasifitacion:"+18",
+        image:"https://ideogram.ai/api/images/direct/twfwoGYbT8C6Dz1zPFfRiQ.jpg",
+    },
+    {
+        id:"5",
+        headerTitle:"Tulum, Q.R.",
+        title:"Jungle Party - Tulum Ruins - Tulum, Q.R.",
+        location:"Tulum Ruins",
+        hour:"11:00pm",
+        date:"15 ",
+        month:"May",
+
+        clasifitacion:"+21",
+        image:"https://ideogram.ai/api/images/direct/WHMD5tbeS1KFiVR7m5wtsQ.jpg",
+    },
+    {
+        id:"6",
+        headerTitle:"Monterrey, N.L.",
+        title:"Rock Fest - Parque Fundidora - Monterrey, N.L.",
+        location:"Parque Fundidora",
+        hour:"08:30pm",
+        date:"20 ",
+        month:"May",
+
+        clasifitacion:"+19",
+        image:"https://ideogram.ai/api/images/direct/7I4O4Tz_T6mjmRE8sR4wwg.jpg",
+    },
+    {
+        id:"7",
+        headerTitle:"Acapulco, Gro.",
+        title:"Beach Volleyball - Playa Condesa - Acapulco, Gro.",
+        location:"Playa Condesa",
+        hour:"06:00pm",
+        date:"25 ",
+        month:"May",
+        clasifitacion:"+16",
+        image:"https://ideogram.ai/api/images/direct/1S0QF_SfRFa2ONQxsZ-jHw.jpg",
+    },
+    {
+        id:"8",
+        headerTitle:"Oaxaca, Oax.",
+        title:"Mezcal Festival - Zócalo de Oaxaca - Oaxaca, Oax.",
+        location:"Zócalo de Oaxaca",
+        hour:"06:30pm",
+        date:"02 ",
+        month:"Jun",
+
+        clasifitacion:"+18",
+        image:"https://ideogram.ai/api/images/direct/OTYRhB5_QqiL9zh83O0PzA.jpg",
+    }
+]
